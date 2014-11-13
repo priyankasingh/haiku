e93540efb5a173fec11ca12805405b17e19bf925
priyankasingh <singh.priyanka.7@gmail.com>
2014-11-13 20:34:25 +0000

    removed punctuation in the end

--- haiku.py
+++ haiku.py
@@ -8,15 +8,16 @@ booklist = []
 with open('book.txt','r') as f:
     for line in f:
         for word in line.split():
-           booklist.append(re.sub(r'\W+', '', word))  
+           booklist.append(word)
 
 print len(booklist)
 
 for i in range(len(booklist)):
-	if i < len(booklist)-1 and booklist[i].lower() not in dictionary:
-		dictionary[booklist[i].lower()]=[booklist[i+1].lower()]
-	elif i < len(booklist)-1:
-		dictionary[booklist[i].lower()].append(booklist[i+1].lower())
+	if booklist[i].isalpha():
+		if i < len(booklist)-1 and booklist[i].lower() not in dictionary:
+			dictionary[booklist[i].lower()]=[re.sub(r'\W+', '',booklist[i+1].lower())]
+		elif i < len(booklist)-1:
+			dictionary[booklist[i].lower()].append(re.sub(r'\W+', '',booklist[i+1].lower()))
 		
 print dictionary
 		
